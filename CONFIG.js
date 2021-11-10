6005774bdafe34497ce18d3e127f65228a32357f
kcheese1505 <93665682+kcheese1505@users.noreply.github.com>
2021-11-10 07:53:15 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -2,7 +2,7 @@ const CONFIGDATA = {
   titleHeader: "Bạn muốn nhận phần quà nào ",
   descriptionHeader:
     "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã nhận quà rồi nhé ",
-  buttonYes: "3000 cái ôm siu bự từ nhóm 1",
+  buttonYes: "3000 cái ôm siu bự từ nhóm 3",
   buttonNo: "Phần quà khác",
   titleModar: "Cảm ơn bạn đã đồng hành cùng tụi mình hehe",
   descriptionModar:
